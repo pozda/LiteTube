598f06c5f42e0d1f1998010b3e21e2e685684ac2
Ivan Pozderac <ivan.pozderac@gmail.com>
2019-12-06 22:54:43 +0100

    husky test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
+const variable = 2
 
 const App: React.FC = () => (
     <div>
         <header>
             <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+                Edit <code>src/App.tsx</code> and save to reload.
             </p>
         </header>
+        <footer>
+            dfdsf
+        </footer>
     </div>
 )
 
